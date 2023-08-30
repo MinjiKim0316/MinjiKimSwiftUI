69e46d670bd277df22470ea2393bc6985ade12c5
MinjiKim0316 <minjikim.6420011@gmail.com>
2023-08-30 15:13:52 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Minji  Kim on 8/30/23.
+//  Copyright © 2023 MinjiKimSwiftUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
