e651ef1c00d6a14a0d0965c6439ae7610e54600d
MinjiKim0316 <108259422+MinjiKim0316@users.noreply.github.com>
2023-11-06 20:58:28 +0900

    upload Nav&Tab by storyboard

---
+++ Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by Minji  Kim on 11/2/23.
+//  Copyright © 2023 MinjiKimSwiftUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
